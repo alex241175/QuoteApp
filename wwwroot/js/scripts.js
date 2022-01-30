262a03dfefc7112ac62cd9b69f054a732a0b3bce
alex241175 <alex2411@gmail.com>
2022-01-30 19:45:55 +0800

    202201301945

--- wwwroot/js/scripts.js
+++ wwwroot/js/scripts.js
@@ -19,3 +19,25 @@ window.getScrollPos = function(){
 window.setScrollPos = function(scrollPos){
     window.scrollTo(0, scrollPos)
 }
+
+window.getInsertAtCursorValue =  function (myField, myValue) {
+    //IE support
+    if (document.selection) {
+        // myField.focus();
+        // sel = document.selection.createRange();
+        // sel.text = myValue;
+        return myValue;
+    }
+    //MOZILLA and others
+    else if (myField.selectionStart || myField.selectionStart == '0') {
+        var startPos = myField.selectionStart;
+        var endPos = myField.selectionEnd;
+        // myField.value = myField.value.substring(0, startPos) + myValue + myField.value.substring(endPos, myField.value.length);
+        return  myField.value.substring(0, startPos)
+            + myValue
+            + myField.value.substring(endPos, myField.value.length);
+    } else {
+        // myField.value += myValue;
+        return myField.value + myValue;
+    }
+}
